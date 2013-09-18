9f8224bca7e83f06272b79c970d0bb57670e4157
Xi Rao <cicirao.hust@gmail.com>
2013-09-17 23:16:21 -0500

    a rough and complete edition

---
+++ scripts/myMusic.js
@@ -0,0 +1,311 @@
+var myMusic = [
+  {
+    "songName": "Young For You",
+    "singer": [
+      "GALA"
+    ],
+    "year": 2004, 
+    "isLike": true,
+    "coverUrl":"url(imgs/0.jpg)",
+    "date": new Date("2013-09-03"),
+    "musicStyle": [
+      "Rock",
+      "Folk"
+    ]
+  },
+  {
+    "songName": "7 Days",
+    "singer": [
+      "Craig David"
+    ],
+    "year": 2001,
+    "isLike": false,
+    "coverUrl":"url(imgs/1.jpg)",
+    "date": new Date("2013-09-03"),
+    "musicStyle": [
+      "Pop",
+      "Blues"
+    ]
+  },
+  {
+    "songName": "Price Tag",
+    "singer": [
+      "Jessie J"
+    ],
+    "year": 2011,
+    "isLike": true,
+    "coverUrl":"url(imgs/2.jpg)",
+    "date": new Date("2013-09-03"),
+    "musicStyle": [
+      "Pop"
+    ]
+  },
+  {
+    "songName": "Fuck You",
+    "singer": [
+      "Lily Allen"
+    ],
+    "year": 2009,
+    "isLike": true,
+    "coverUrl":"url(imgs/3.jpg)",
+    "date": new Date("2013-09-03"),
+    "musicStyle": [
+      "Pop"
+    ]
+  },
+  {
+    "songName": "That's Not My Name",
+    "singer": [
+      "The Ting Tings"
+    ],
+    "year": 2008,
+    "isLike": true,
+    "coverUrl":"url(imgs/4.jpg)",
+    "date": new Date("2013-09-03"),
+    "musicStyle": [
+      "Electronic",
+      "Rock"
+    ]
+  },
+  {
+    "songName": "Gotta Have You",
+    "singer": [
+      "The Weepies"
+    ],
+    "year": 2006,
+    "isLike": true,
+    "coverUrl":"url(imgs/5.jpg)",
+    "date": new Date("2013-08-28"),
+    "musicStyle": [
+      "Folk"
+    ]
+  },
+  {
+    "songName": "Free Loop",
+    "singer": [
+      "Daniel Powter"
+    ],
+    "year": 2006,
+    "isLike": false,
+    "coverUrl":"url(imgs/6.jpg)",
+    "date": new Date("2013-09-02"),
+    "musicStyle": [
+      "Pop"
+    ]
+  },
+  {
+    "songName": "I'm Yours",
+    "singer": [
+      "Jason Mraz"
+    ],
+    "year": 2008,
+    "isLike": true,
+    "coverUrl":"url(imgs/7.jpg)",
+    "date": new Date("2013-09-02"),
+    "musicStyle": [
+      "Folk"
+    ]
+  },
+  {
+    "songName": "Count On Me",
+    "singer": [
+      "Bruno Mars"
+    ],
+    "year": 2010,
+    "isLike": false,
+    "coverUrl":"url(imgs/8.jpg)",
+    "date": new Date("2013-09-02"),
+    "musicStyle": [
+      "R&B",
+      "Pop"
+    ]
+  },
+  {
+    "songName": "Best Of Me",
+    "singer": [
+      "Daniel Powter"
+    ],
+    "year": 2008,
+    "isLike": false,
+    "coverUrl":"url(imgs/9.jpg)",
+    "date": new Date("2013-09-02"),
+    "musicStyle": [
+      "Pop"
+    ]
+  },
+  {
+    "songName": "Lost In Paradise",
+    "singer": [
+      "Joanna"
+    ],
+    "year": 2008,
+    "isLike": false,
+    "coverUrl":"url(imgs/10.jpg)",
+    "date": new Date("2013-09-02"),
+    "musicStyle": [
+      "Jazz"
+    ]
+  },
+  {
+    "songName": "Moves like Jagger",
+    "singer": [
+      "Maroon 5"
+    ],
+    "year": 2012,
+    "isLike": false,
+    "coverUrl":"url(imgs/11.jpg)",
+    "date": new Date("2013-09-01"),
+    "musicStyle": [
+      "Pop"
+    ]
+  },
+  {
+    "songName": "So What",
+    "singer": [
+      "P!nk"
+    ],
+    "year": 2008,
+    "isLike": true,
+    "coverUrl":"url(imgs/12.jpg)",
+    "date": new Date("2013-09-01"),
+    "musicStyle": [
+      "Pop",
+      "Rock"
+    ]
+  },
+  {
+    "songName": "Lucky",
+    "singer": [
+      "Jason Mraz",
+      "Colbie Caillat"
+    ],
+    "year": 2008,
+    "isLike": true,
+    "coverUrl":"url(imgs/13.jpg)",
+    "date": new Date("2013-08-31"),
+    "musicStyle": [
+      "Folk"
+    ]
+  },
+  {
+    "songName": "Say Hello",
+    "singer": [
+      "Rosie Thomas"
+    ],
+    "year": 2007,
+    "isLike": true,
+    "coverUrl":"url(imgs/14.jpg)",
+    "date": new Date("2013-08-31"),
+    "musicStyle": [
+      "Folk"
+    ]
+  },
+  {
+    "songName": "Just You And Me",
+    "singer": [
+      "Zee Avi"
+    ],
+    "year": 2009,
+    "isLike": true,
+    "coverUrl":"url(imgs/15.jpg)",
+    "date": new Date("2013-08-30"),
+    "musicStyle": [
+      "Jazz"
+    ]
+  },
+  {
+    "songName": "Take A Bow",
+    "singer": [
+      "Rihanna"
+    ],
+    "year": 2008,
+    "isLike": true,
+    "coverUrl":"url(imgs/16.jpg)",
+    "date": new Date("2013-08-30"),
+    "musicStyle": [
+      "R&B",
+      "Pop"
+    ]
+  },
+  {
+    "songName": "The Lazy Song",
+    "singer": [
+      "Bruno Mars"
+    ],
+    "year": 2010,
+    "isLike": true,
+    "coverUrl":"url(imgs/17.jpg)",
+    "date": new Date("2013-08-29"),
+    "musicStyle": [
+      "Folk"
+    ]
+  },
+  {
+    "songName": "Bitter Heart",
+    "singer": [
+      "Zee Avi"
+    ],
+    "year": 2009,
+    "isLike": false,
+    "coverUrl":"url(imgs/18.jpg)",
+    "date": new Date("2013-08-29"),
+    "musicStyle": [
+      "Jazz",
+      "Folk"
+    ]
+  },
+  {
+    "songName": "Need You Now",
+    "singer": [
+      "Lady Antebellum"
+    ],
+    "year": 2011,
+    "isLike": false,
+    "coverUrl":"url(imgs/19.jpg)",
+    "date": new Date("2013-08-28"),
+    "musicStyle": [
+      "Pop"
+    ]
+  },
+  {
+    "songName": "Good Time",
+    "singer": [
+      "Carly Rae Jepsen",
+      "Owl City"
+    ],
+    "year": 2012,
+    "isLike": false,
+    "coverUrl":"url(imgs/20.jpg)",
+    "date": new Date("2013-08-28"),
+    "musicStyle": [
+      "Pop"
+    ]
+  },
+  {
+    "songName": "Never Know",
+    "singer": [
+      "Jack Johnson"
+    ],
+    "year": 2005,
+    "isLike": false,
+    "coverUrl":"url(imgs/21.jpg)",
+    "date": new Date("2013-08-31"),
+    "musicStyle": [
+      "Folk"
+    ]
+  },
+  {
+    "songName": "Fallin' For You",
+    "singer": [
+      "Colbie Caillat"
+    ],
+    "year": 2009,
+    "isLike": false,
+    "coverUrl":"url(imgs/22.jpg)",
+    "date": new Date("2013-08-31"),
+    "musicStyle": [
+      "Folk",
+      "Pop"
+    ]
+  }
+]
